3e741d79c3f79d67ee2c877debdd7eee5f947465
Wooyoung Chung <yakle1218@gmail.com>
2014-01-16 16:14:24 -0500

    Ready to implement heap

--- Heap/intoxicated/Objective-C/Heap.m
+++ Heap/intoxicated/Objective-C/Heap.m
@@ -5,20 +5,66 @@
 //  Created by R3alFr3e on 1/7/14.
 //  Copyright (c) 2014 intoxicated. All rights reserved.
 //
+//  Binary Heap implementation
 
 #import "Heap.h"
+#import "BinaryTree.h"
 
-@interface Heap
+@interface Heap ()
 @property (nonatomic, strong) NSMutableArray * heapArray;
 @property (nonatomic, assign) BOOL isFixedSize;
+@property (nonatomic, strong) BinaryTree * tree;
 
 - (void)max_heapify:(NSInteger)index;
 - (void)min_heapify:(NSInteger)index;
 - (void)swapWith:(NSInteger)index otherIndex:(NSInteger)otherIndex;
 - (NSInteger)left:(NSInteger)index;
 - (NSInteger)right:(NSInteger)index;
+
 @end
 
 @implementation Heap
 
+- (id)init
+{
+    
+}
+- (id)initWithOption:(NSInteger)op
+{
+    
+}
+- (id)initWithItems:(NSArray *)array
+{
+    
+}
+
+- (void)enqueue:(id)obj
+{
+    
+}
+- (id)dequeue
+{
+    
+}
+- (id)popMin
+{
+    
+}
+- (id)popMax
+{
+    
+}
+
+- (void)merge:(Heap *)otherHeap
+{
+    
+}
+- (void)print
+{
+    
+}
+- (NSInteger)size
+{
+    
+}
 @end
